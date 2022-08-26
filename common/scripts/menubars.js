a7c094a2032d3e53276ed0c1dd729b717639bfa0
Chris Urffer <c.urffer@live.com>
2022-08-25 21:02:34 -0400

    updated structure again

--- common/scripts/menubars.js
+++ common/scripts/menubars.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	$("head").prepend(
-    '<link rel="shortcut icon" href="mobfall-icon.png" type="image/x-icon" />'
+    '<link rel="shortcut icon" href="common/images/mobfall-icon.png" type="image/x-icon" />'
     );
     $("body").prepend(
-    '<div id="topbanner" style="background-image: url(index_resources/Mobfall_Website-Banner-1920x250.jpg);"></div>'+
+    '<div id="topbanner" style="background-image: url(index/Mobfall_Website-Banner-1920x250.jpg);"></div>'+
     '<nav id="topbar" data-spy="affix" data-offset-top="250">'+
         '<ul id="nav">'+
-            '<a href="/index/index.html"><li id="homeTopbar" class="menuButton">Home</li></a>'+
-            '<a href="/download/index.html"><li id="downloadTopbar" class="menuButton">Download</li></a>'+
-            '<a href="/donate/index.html"><li id="donateTopbar" class="menuButton">Donate</li></a>'+
+            '<a href="index.html"><li id="homeTopbar" class="menuButton">Home</li></a>'+
+            '<a href="download.html"><li id="downloadTopbar" class="menuButton">Download</li></a>'+
+            '<a href="donate.html"><li id="donateTopbar" class="menuButton">Donate</li></a>'+
         '</ul>'+
     '</nav>'
     );
